a7b764d482264f9ef102c50a5c6f5e255c5c8b0c
hjcarlsen <152008464+hjcarlsen@users.noreply.github.com>
2026-01-30 14:41:50 +0100

    teoretisk LC filter frekvensresponds

---
+++ Lab_1–måleoppsett/teoretisk_frekvesnrespongLC-filter.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Component values (example, can be changed)
+L = 100e-3      # 100 mH
+C1 = 100e-6     # 100 uF
+C2 = 470e-6     # 470 uF
+C3 = 100e-9     # 100 nF
+
+# Total equivalent capacitance
+C_tot = (C1 * (C2 + C3)) / (C1 + (C2 + C3))
+
+# Frequency range
+f = np.logspace(2, 5, 1000)  # 100 Hz to 100 kHz
+w = 2 * np.pi * f
+s = 1j * w
+
+# System function H(s)
+H = 1 / (1 + (s**2) * L * C_tot)
+
+# Magnitude in dB
+H_mag_db = 20 * np.log10(np.abs(H))
+
+# Plot
+plt.figure()
+plt.semilogx(f, H_mag_db)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Magnitude |H(jω)| [dB]")
+plt.title("Theoretical Frequency Response of LC Low-Pass Filter")
+plt.grid(True)
+plt.show()
